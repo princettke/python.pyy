326e4265dd5a5875989003a15c159fca0100418e
princettke <kushwahprince486@gmail.com>
2026-06-19 01:20:34 +0530

    commit 1 files to main

---
+++ indexing.list.py
@@ -0,0 +1,12 @@
+list=["fruits","vegetable",7,6,5,4] #list mutable means changes are done
+print(list[4])
+list[1]="prince" # in list you can change directlly 
+print(list)
+print(list[1:5]) 
+list.append("harry") # for add elements in tuple
+print(list)
+l1=[1,3,4,2,7,6,8,10,9]
+l1.sort() #sort in order in inreasing order 
+print(l1)
+l1.reverse()
+print(l1)
