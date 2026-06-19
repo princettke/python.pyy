e74d3f4de233680fc79dad615776e3c0842ef42e
princettke <kushwahprince486@gmail.com>
2026-06-19 22:30:34 +0530

    commit 1 file to main

--- indexing.list.py
+++ indexing.list.py
@@ -5,8 +5,13 @@ print(list)
 print(list[1:5]) 
 list.append("harry") # for add elements in tuple
 print(list)
-l1=[1,3,4,2,7,6,8,10,9]
+l1=[1,3,4,2,7,5,6,8,10,9]
 l1.sort() #sort in order in inreasing order 
 print(l1)
 l1.reverse()
+print(l1)
+#for insert in any place 
+l1.insert(3,3333)
+print(l1)
+print(l1.pop(4)) # for pop 
 print(l1)
