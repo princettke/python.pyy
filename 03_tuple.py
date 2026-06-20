5614489328345872cf66f968ec6dcb3ba44646a3
princettke <kushwahprince486@gmail.com>
2026-06-20 22:55:35 +0530

    commit 1 files to main

---
+++ 03_tuple.py
@@ -0,0 +1,15 @@
+a=(1,2,3,4,5)
+print(type(a))
+
+#tuple is imutable
+#tuple methods
+a=(1,45,342,3424,False,"Rohan","Shivam")
+print(a)
+no=a.count(45)
+print(no)
+print(a.index(1))
+#merging two tuple
+tuple1=(1,2,3)
+tuple2=(4,5,6)
+a=tuple1+tuple2
+print(a)
