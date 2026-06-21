52a190a95dcd9071bd11ce6831d77bcd8e0d6b48
princettke <kushwahprince486@gmail.com>
2026-06-21 23:09:49 +0530

    commit 1 file to main

---
+++ Dictnory.py
@@ -0,0 +1,15 @@
+marks ={
+  "kushal":100,
+  "harrypoter":56,
+  "Rohan":34,
+  "list":[1,2,3]
+}
+print(marks,type(marks))
+print(marks["kushal"])
+print(marks["list"])
+#dictnory
+#its is unorderd
+#it is mutable--means changeable
+#it is indexed
+#cannot contain duplicate key 
+ 
