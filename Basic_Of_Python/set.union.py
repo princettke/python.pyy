1b222859fcfddd49d8fe109ac071e8781b881c33
princettke <kushwahprince486@gmail.com>
2026-06-27 22:51:11 +0530

    commit 1 file to main

--- Basic_Of_Python/set.union.py
+++ Basic_Of_Python/set.union.py
@@ -1,4 +1,4 @@
 s1 = {1,45,6}
 s2 = {7,8,1,78}
 print(s1.union(s2)) #Union (| or union()) do sets ke saare unique elements ko ek naya set mein combine karta hai.
-print()
+print(s1.intersection(s2)) # print common value of intersection 
