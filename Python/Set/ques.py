afb7734cdda8ec6eda463cecb7455bef5434da58
princettke <kushwahprince486@gmail.com>
2026-06-28 22:54:31 +0530

    commit 1 file to main

---
+++ Python/Set/ques.py
@@ -0,0 +1,22 @@
+#write a program to input eight unique number from the user and display all the unique numbers once 
+s=set()
+n=input("enter your number:")
+s.add(int(n))
+n=input("enter your number:")
+s.add(int(n))
+n=input("enter your number:")
+s.add(int(n))
+n=input("enter your number:")
+s.add(int(n))
+n=input("enter your number:")
+s.add(int(n))
+n=input("enter your number:")
+s.add(int(n))
+n=input("enter your number:")
+s.add(int(n))
+n=input("enter your number:")
+s.add(int(n))
+print(s)
+
+
+
