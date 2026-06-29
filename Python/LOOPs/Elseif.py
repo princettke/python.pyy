56a11a8dc63bbc0371515044c7ba79b390bb724d
princettke <kushwahprince486@gmail.com>
2026-06-29 23:49:33 +0530

    commit 1 file to main

---
+++ Python/LOOPs/Elseif.py
@@ -0,0 +1,7 @@
+age=int(input("enter your age "))
+if(age>18):
+  print("your are eligile")
+elif(age<0):
+  print("your are enter negative age sorry i will not proceded ")
+else:
+  print("your are not eligible ")
