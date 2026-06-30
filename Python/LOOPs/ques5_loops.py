cb948cc5bd2b5b873307f091a1cdeb93ed94c8a7
princettke <kushwahprince486@gmail.com>
2026-06-30 20:28:18 +0530

    commit 1 file to main

---
+++ Python/LOOPs/ques5_loops.py
@@ -0,0 +1,7 @@
+l=["prince","Rohan","kushal","amarya"]
+name = input("enter your name ")
+if(name in l):
+  print("name is present")
+else:
+  print("name is not present")
+  # next ques is about lower 
