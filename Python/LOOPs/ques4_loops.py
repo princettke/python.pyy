70dea8d671fcd5c0d4ef9c24d5c89ded48fbe8c3
princettke <kushwahprince486@gmail.com>
2026-06-30 20:19:22 +0530

    commit 1 file to main

---
+++ Python/LOOPs/ques4_loops.py
@@ -0,0 +1,6 @@
+# Write a program to find whether a given username contains less than 10 characters or not.
+username= input("enter your username ")
+if(len(username)<10):
+  print("your usernname conatins less than 10 character")
+else:
+  print("your user name containn more than 10 character")
